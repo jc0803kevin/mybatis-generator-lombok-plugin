559ef251a6b07d00244e50432c20fc3e95cdddf5
jc0803kevin <jc0803kevin@163.com>
2020-06-08 18:34:26 +0800

    release 1.0

--- src/main/java/com/kevin/generator/api/LombokPlugin.java
+++ src/main/java/com/kevin/generator/api/LombokPlugin.java
@@ -3,15 +3,10 @@ package com.kevin.generator.api;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
-import org.mybatis.generator.api.dom.java.Interface;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.java.*;
 
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Properties;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @Author kevin
@@ -21,9 +16,14 @@ import java.util.Properties;
 public class LombokPlugin extends PluginAdapter {
 
     private final Collection<Annotations> annotations;
+    private String currentDateStr;
+    private String author;
+
+    private String supperClass;
 
     public LombokPlugin() {
         annotations = new LinkedHashSet<Annotations>(Annotations.values().length);
+        currentDateStr = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date());
     }
 
     @Override
@@ -36,6 +36,37 @@ public class LombokPlugin extends PluginAdapter {
             TopLevelClass topLevelClass,
             IntrospectedTable introspectedTable
     ) {
+
+
+        // 添加domain的注释
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * @author   " + author);
+        topLevelClass.addJavaDocLine(" * @date   " + currentDateStr);
+        topLevelClass.addJavaDocLine(" */");
+
+        // 设置父类
+        if (supperClass != null) {
+            topLevelClass.setSuperClass(new FullyQualifiedJavaType(supperClass));
+        }
+
+        // 将需要忽略生成的属性过滤掉
+        List<Field> fields = topLevelClass.getFields();
+
+        // 给每个字段加注释
+        for (Field field : fields) {
+            StringBuilder fieldSb = new StringBuilder();
+            field.addJavaDocLine("/**");
+            fieldSb.append(" * ");
+            String fieldName = field.getName(); // java字段名是驼峰的，需要转成下划线分割
+            String underlineFieldName = camelToUnderline(fieldName);
+            IntrospectedColumn introspectedColumn = introspectedTable.getColumn(underlineFieldName);
+            if (null != introspectedColumn) {
+                fieldSb.append(introspectedColumn.getRemarks());
+            }
+            field.addJavaDocLine(fieldSb.toString().replace("\n", " "));
+            field.addJavaDocLine(" */");
+        }
+
         addAnnotations(topLevelClass);
         return true;
     }
@@ -45,6 +76,7 @@ public class LombokPlugin extends PluginAdapter {
             TopLevelClass topLevelClass,
             IntrospectedTable introspectedTable
     ) {
+
         addAnnotations(topLevelClass);
         return true;
     }
@@ -66,6 +98,7 @@ public class LombokPlugin extends PluginAdapter {
             IntrospectedTable introspectedTable,
             ModelClassType modelClassType
     ) {
+        //不生成getter
         return false;
     }
 
@@ -77,6 +110,7 @@ public class LombokPlugin extends PluginAdapter {
             IntrospectedTable introspectedTable,
             ModelClassType modelClassType
     ) {
+        //不生成setter
         return false;
     }
 
@@ -93,7 +127,7 @@ public class LombokPlugin extends PluginAdapter {
 
         //@Data is default annotation
         annotations.add(Annotations.DATA);
-
+        author = properties.getProperty("author");
         for (String annotationName : properties.stringPropertyNames()) {
             if (annotationName.contains(".")) {
                 // Not an annotation name
@@ -122,6 +156,14 @@ public class LombokPlugin extends PluginAdapter {
         }
     }
 
+
+    /**
+     * @Author kevin
+     * @Description Mapper文件的注释
+     * @Date Created on 2020/6/8 16:50
+     * @param
+     * @return
+     */
     @Override
     public boolean clientGenerated(
             Interface interfaze,
@@ -131,6 +173,38 @@ public class LombokPlugin extends PluginAdapter {
         interfaze.addImportedType(new FullyQualifiedJavaType(
                 "org.apache.ibatis.annotations.Mapper"));
         interfaze.addAnnotation("@Mapper");
+
+        // Mapper文件的注释
+        interfaze.addJavaDocLine("/**");
+        interfaze.addJavaDocLine("* Created by Mybatis Generator on " + currentDateStr);
+        interfaze.addJavaDocLine("*/");
+
         return true;
+    }
+
+    /**
+     *
+     * @Title: camelToUnderline
+     * @Description: 把 java 驼峰的变量变为下划线的
+     * @param fieldName
+     * @return String
+     */
+    private static String camelToUnderline(String fieldName) {
+        StringBuilder result = new StringBuilder();
+        if (fieldName != null && fieldName.length() > 0) {
+            // 将第一个字符处理成大写
+            result.append(fieldName.substring(0, 1).toUpperCase());
+            // 循环处理其余字符
+            for (int i = 1; i < fieldName.length(); i++) {
+                String s = fieldName.substring(i, i + 1);
+                // 在大写字母前添加下划线
+                if (s.equals(s.toUpperCase()) && !Character.isDigit(s.charAt(0))) {
+                    result.append("_");
+                }
+                // 其他字符直接转成大写
+                result.append(s.toUpperCase());
+            }
+        }
+        return result.toString();
     }
 }
